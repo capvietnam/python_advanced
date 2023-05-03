6252df509dcb0f89bf55cdb18218da1c1119a4c7
capvietnam <zenakorneev940@gmail.com>
2023-05-04 02:13:16 +0300

    3 модуль готово

--- module_03_ci_culture_beginning/homework/hw1/hello_word_with_day.py
+++ module_03_ci_culture_beginning/homework/hw1/hello_word_with_day.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
--- module_03_ci_culture_beginning/homework/hw1/tests/test_week.py
+++ module_03_ci_culture_beginning/homework/hw1/tests/test_week.py
@@ -1,7 +1,19 @@
+
+from datetime import datetime
 import unittest
 from freezegun import freeze_time
 
 from hello_word_with_day import app
+
+GREETINGS = (
+    'Хорошего понедельника',
+    'Хорошего вторника',
+    'Хорошей среды',
+    'Хорошего четверга',
+    'Хорошей пятницы',
+    'Хорошей субботы',
+    'Хорошего воскресенья'
+)
 
 
 class TestWeek(unittest.TestCase):
@@ -12,8 +24,15 @@ class TestWeek(unittest.TestCase):
         self.app = app.test_client()
         self.base_url = '/hello-world/'
 
-    @freeze_time('2022-02-23')
     def test_day_of_the_week_verification_test(self):
+        name = 'jane'
+        response = self.app.get(self.base_url + name)
+        response_text = response.data.decode()
+        correct_answer_str = f'Привет, {name}. {GREETINGS[datetime.today().weekday()]}!'
+        self.assertTrue(correct_answer_str in response_text)
+
+    @freeze_time('2022-02-23')
+    def test_day_verification_test(self):
         name = 'jane'
         response = self.app.get(self.base_url + name)
         response_text = response.data.decode()
