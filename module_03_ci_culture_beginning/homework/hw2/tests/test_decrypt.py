278a94ad2683a63e0875a74a036154afb892988f
capvietnam <zenakorneev940@gmail.com>
2023-05-04 22:25:40 +0300

    3 модуль готово

--- module_03_ci_culture_beginning/homework/hw2/tests/test_decrypt.py
+++ module_03_ci_culture_beginning/homework/hw2/tests/test_decrypt.py
@@ -1,34 +1,34 @@
 import unittest
 from decrypt import decrypt
 
+TestAnswer0 = {'абра-кадабра': 'абра-кадабра', '.': ''}
+TestAnswer1 = {'abra-kadabra.': 'abra-kadabra', 'abraa..-.kadabra': 'abra-kadabra', 'abrau...-kadabra': 'abra-kadabra'}
+TestAnswer2 = {'абраа..-кадабра': 'абра-кадабра', 'абра--..кадабра': 'абра-кадабра'}
+TestAnswer3 = {'абраа..-.кадабра': 'абра-кадабра', 'абрау...-кадабра': 'абра-кадабра', '1..2.3': '23'}
+TestAnswerMany = {'абра........': '', 'абр......a.': 'a', '1.......................': ''}
+
 
 class TestDecrypt(unittest.TestCase):
 
     def test_no_dot(self):
-        self.assertEqual(decrypt('абра-кадабра'), 'абра-кадабра')
-        self.assertEqual(decrypt('.'), '')
+        for key, value in TestAnswer0.items():
+            self.assertEqual(decrypt(key), value)
 
     def test_one_dot(self):
-        self.assertEqual(decrypt('abra-kadabra.'), 'abra-kadabra')
-        self.assertEqual(decrypt('abraa..-.kadabra'), 'abra-kadabra')
-        self.assertEqual(decrypt('abrau...-kadabra'), 'abra-kadabra')
+        for key, value in TestAnswer1.items():
+            self.assertEqual(decrypt(key), value)
 
     def test_two_dot(self):
-        self.assertEqual(decrypt('абраа..-кадабра'), 'абра-кадабра')
-        self.assertEqual(decrypt('абра--..кадабра'), 'абра-кадабра')
+        for key, value in TestAnswer2.items():
+            self.assertEqual(decrypt(key), value)
 
     def test_three_dot(self):
-        self.assertEqual(decrypt('абраа..-.кадабра'), 'абра-кадабра')
-        self.assertEqual(decrypt('абрау...-кадабра'), 'абра-кадабра')
-        self.assertEqual(decrypt('1..2.3'), '23')
+        for key, value in TestAnswer3.items():
+            self.assertEqual(decrypt(key), value)
 
     def test_many_dot(self):
-        self.assertEqual(decrypt('абра........'), '')
-        self.assertEqual(decrypt('абр......a.'), 'a')
-        self.assertEqual(decrypt('1.......................'), '')
-    # TODO стоило поместить все исходные данные для тестирования в словарь (по одному для каждой группы) или список
-    #  списков и выполнять все "кейсы" в цикле
-
+        for key, value in TestAnswerMany.items():
+            self.assertEqual(decrypt(key), value)
 
 if __name__ == '__main__':
     unittest.main()
