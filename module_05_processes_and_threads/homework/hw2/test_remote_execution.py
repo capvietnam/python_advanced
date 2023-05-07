741536ad7bbdad1341f4f04dc906486ac88c4ae5
cap <zenakorneev940@gmail.com>
2023-05-07 23:47:43 +0300

    module 5 2

--- module_05_processes_and_threads/homework/hw2/test_remote_execution.py
+++ module_05_processes_and_threads/homework/hw2/test_remote_execution.py
@@ -1,5 +1,32 @@
 import unittest
-from remote_execution import app
+from remote_execution import run_python_code_in_subprocess
+
+
+class TestRunPythonCodeInSubprocess(unittest.TestCase):
+
+    def test_returns_stdout_and_stderr(self):
+        code = "print('hello')\nprint('world')"
+        timeout = 10
+        stdout, stderr, killed = run_python_code_in_subprocess(code, timeout)
+        self.assertEqual(stdout, "hello\nworld\n")
+        self.assertEqual(stderr, "")
+        self.assertEqual(killed, False)
+
+    def test_returns_stderr_on_syntax_error(self):
+        code = "print('hello' +)\nprint('world')"
+        timeout = 10
+        stdout, stderr, killed = run_python_code_in_subprocess(code, timeout)
+        self.assertEqual(stdout, "")
+        self.assertIn("SyntaxError", stderr)
+        self.assertEqual(killed, False)
+
+    def test_kills_process_on_timeout(self):
+        code = "while True: pass"
+        timeout = 1
+        stdout, stderr, killed = run_python_code_in_subprocess(code, timeout)
+        self.assertEqual(stdout, "")
+        self.assertEqual(stderr, "")
+        self.assertEqual(killed, True)
 
 
 if __name__ == '__main__':
